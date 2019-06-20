d494cfa1624b32f18323aeea16769e2305c24d19
Tyler Williams <williams13@grinnell.edu>
2019-06-20 19:31:35 +0000

    hopefully this works

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,4 @@
 class StaticPagesController < ApplicationController
-  def about
-  end
-
-  def csprojects
-  end
-
-  def videoprojects
-  end
-  
   def blog
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,8 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
-  test "should get about" do
-    get :about
-    assert_response :success
-  end
-
-  test "should get csprojects" do
-    get :csprojects
-    assert_response :success
-  end
-
-  test "should get videoprojects" do
-    get :videoprojects
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get blog" do
+    get static_pages_blog_url
     assert_response :success
   end
 
